5193e84ea3a2ca9b26a3bb2b41e647bd2a7be315
ntswaki1934 <ntswaki1934@gmail.com>
2024-12-01 11:19:06 +0200

    config files

---
+++ website/tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        silver: '#C0C0C0',
+        black: '#000000',
+      },
+    },
+  },
+  plugins: [],
+};
+
